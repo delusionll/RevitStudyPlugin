eb6303f11f5814a66286e0582ca5ff37015a3942
delusion <lev.zernov@yandex.ru>
2022-11-28 13:07:00 +0300

    Добавьте файлы проекта.

---
+++ RevitStudyPlugin/StartClassPlugin.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Autodesk.Revit.DB;
+using Autodesk.Revit.UI;
+
+namespace RevitStudyPlugin
+{
+    public class StartClassPlugin : IExternalCommand
+    {
+    }
+}
